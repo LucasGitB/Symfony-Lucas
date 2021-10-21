3506cf12a3586d60df4c6a4ad04abe196e71b262
LucasGitB <77726940+LucasGitB@users.noreply.github.com>
2021-10-21 21:27:14 +0200

    Initial commit

---
+++ src/Controller/ProductController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Product;
+use App\Form\ProductType;
+
+class ProductController extends AbstractController
+{
+    /**
+     * @Route("/product", name="product")
+     */
+    public function index(Request $request): Response
+    {
+        $em      = $this->getDoctrine()->getManager();
+        $product = new Product();
+        $form    = $this->createForm(ProductType::class, $product);
+
+        if ($request->isMethod('POST') && $form->handleRequest($request)->isValid()){
+            $em->persist($product);
+            $em->flush();
+        }
+
+        return $this->render('product/index.html.twig', [
+            'form' => $form->createView(),
+        ]);
+    }
+
+ /**
+     * @Route("/producthome", name="producth")
+     */
+    public function accueil(): Response
+    {
+
+
+        return $this->render('product/accueil.html.twig');
+    }
+
+    /**
+     * @Route("/producta", name="article")
+     */
+    public function showProduct(){
+
+        $product = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->findAll();
+
+        if(!$product){
+            throw $this->createNotFoundException('Pas de produit !');
+        }
+        return $this->render('product/all.html.twig', [
+            'product' => $product
+          ]);
+    }
+    /**
+     * @Route("/productDescription/{id}", name="article2")
+     */
+    public function showProductDescription($id){
+
+        $product = $this->getDoctrine()
+        ->getRepository(Product::class)
+        ->find($id);
+
+        if(!$product){
+            throw $this->createNotFoundException('Pas de produit !');
+        }
+        return $this->render('product/productdescription.html.twig', [
+            'product' => $product
+          ]);
+
+        //return new Response($product->getName());
+    }
+}
